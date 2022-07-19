ba20e6d2990a6a000653deac7cfacaedadab1ceb
Akashdeep2125 <71831879+Akashdeep2125@users.noreply.github.com>
2022-07-19 08:51:28 -0700

    Update Comp.java

--- Comp.java
+++ Comp.java
@@ -1,4 +1,54 @@
-package com.oopshiding;
+class Heart{
+
+	public void beats()
+package com.oopshiding;	{
+
+		System.out.println("Every Human has heart");
+
+	}
+
+}
+
+class Human{
+
+private final Heart heart;
+
+Human(Heart heart){
+
+	this.heart=heart;
+
+}
+
+public void move() 
+
+{
+
+	heart.beats();
+
+	System.out.println("Human needs exercise to keep heart healthy");
+
+}
+
+}
+
+public class HumanComposition {
+
+
+
+	public static void main(String[] args) {
+
+		Heart heart = new Heart();
+
+		Human human = new Human(heart);
+
+		human.move();
+
+	}
+
+
+
+}
+
 
 import java.util.ArrayL ist;
 import java.util.LinkedList;
